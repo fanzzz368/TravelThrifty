cbbfbd001d805871739bd8ddaf2daa94055a40f8
Fanny Zheng <91427854+fanzzz368@users.noreply.github.com>
2023-06-24 20:15:10 -0400

    WIP start frontend for the homepage

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
-def hello():
-    return {'message': 'hello word!'}
+def home_page():
+    return render_template('index.html')
 
+
+@app.route('/questionnaire')
+def questionnaire_page():
+    return render_template('questionnaire.html')
+
+
+@app.route('/creators')
+def creator_page():
+    return render_template('creators.html')
+
+@app.route('/cindy')
+def cindy():
+    return render_template('TravelThrifty.html')
 
 if __name__ == '__main__':
     app.run()
